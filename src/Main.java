0b61fcb6852a0c1bd622ece19ee22024be27b1ab
User <alymbekovnursultan232@gmail.com>
2024-11-07 13:41:21 +0600

    commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.imageio.IIOException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
